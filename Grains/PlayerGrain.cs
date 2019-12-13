af35d141039da9c5fa6d4950be04821a3f6e0ae4
Christian Røssel <pbh816@alumni.ku.dk>
2019-12-13 11:18:09 +0100

    Something that maybe makes sense?

--- Grains/PlayerGrain.cs
+++ Grains/PlayerGrain.cs
@@ -23,23 +23,42 @@ namespace Grains
         public override Task OnActivateAsync()
         { 
             throw new NotImplementedException();
+            //get last state of player before deactivation (from memory)
+            //Give state to new actor
+            //Initialize new Actor
+
         }
 
         public override Task OnDeactivateAsync() 
         {
             throw new NotImplementedException();
+            //remember state when a player is deactivated
         }
 
         Task IPlayer.Initialize(List<Guid> playerIds, bool isHoldingBall)
         {
             foreach (Guid id in playerIds) {
                 IPlayer player = GrainFactory.GetGrain<IPlayer>(id);
+                //if {} Implemet whether or not the Actor has a state - if not, initialize otherwise go to isHoldingBall
+                //playerIds.Add(id);
                 if (isHoldingBall) {
-                    player.ReceiveBall();
+                    //player.ReceiveBall();
+                    State.BallIds = new List<Guid>();
+                    State.BallIds.Add(new Guid());
+                    // HoldOrPassBall(null); Maybe do not hold or pass ball here
+                }
+                else
+                {
+                    State.BallIds = new List<Guid>();
+                    State.BallIds.Add(new Guid());
+                    // player.ReceiveBall(new Guid()); Maybe the balls shouldn't be distributed here
                 }
             }
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
+
+
+               
 
         Task IPlayer.ReceiveBall(Guid ballId)
         {
@@ -62,8 +81,9 @@ namespace Grains
         ///  period or to pass the ball.
         /// </summary>
         private Task HoldOrPassBall(object arg)
-        {
+        { 
             throw new NotImplementedException();
+            // Await that others receive balls
         }
 
         /// <summary>
