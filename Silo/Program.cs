9bc780517b02fcdcb6c02612db2fff5feb9151b1
Tobias Floor <sxr297@alumni.ku.dk>
2019-12-16 17:35:39 +0100

    Added git ignore for generated files

---
+++ Silo/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System.Collections.Generic;
+
+namespace OrleansBasics
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                var host = await StartSilo();
+
+
+                var client = await ConnectClient();
+
+                Console.WriteLine("\n\n Press Enter to start process...\n\n");
+                Console.ReadLine();
+
+                // Create Player instance - n players
+                var player = client.GetGrain<IPlayer>(Guid.NewGuid());
+
+                //A player with a new GUID is being initialized where he is not holding a ball
+                await player.Initialize(new List<Guid>(), false);
+
+                Console.WriteLine("Give the player a ball...");
+                Console.ReadLine();
+                //Give Player a ball (new ball) - k balls
+                //await player.ReceiveBall(Guid.NewGuid());
+
+                
+
+                Console.WriteLine("Everything has been completed succesfully, bitch!");
+                Console.ReadLine();
+
+
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            // define the cluster configuration
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "OrleansBasics";
+                })
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(PlayerGrain).Assembly).WithReferences())
+                .ConfigureLogging(logging => logging.AddConsole())
+                .AddAdoNetGrainStorage("OrleansStorage", options =>
+                {
+                    options.Invariant = "Npgsql";
+                    options.ConnectionString = "host=localhost;database=OrleansStorage;password=postgres;username=postgres";
+                    options.UseJsonFormat = true;
+                })
+               .UseInMemoryReminderService(); 
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+
+
+        private static async Task<IClusterClient> ConnectClient()
+        {
+            IClusterClient client;
+            client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "OrleansBasics";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Client successfully connected to silo host \n");
+            return client;
+        }
+    }
+}
--- Tests/IPlayerTests.cs
+++ Tests/IPlayerTests.cs
@@ -1,9 +1,5 @@
-using GrainInterfaces;
-using Grains;
-using Orleans;
 using Orleans.TestingHost;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -12,50 +8,20 @@ namespace XUnitTests
 {
     public class IPlayerTests
     {
-        private ClusterFixture fixture;
         private readonly TestCluster _cluster;
         private readonly ITestOutputHelper _testOutputHelper;
 
-        public IPlayerTests(/*ClusterFixture fixture, */ITestOutputHelper testOutputHelper)
+        public IPlayerTests(ClusterFixture fixture, ITestOutputHelper testOutputHelper)
         {
             _testOutputHelper = testOutputHelper;
-            fixture = new ClusterFixture();
             _cluster = fixture.Cluster;
         }
 
+        //TODO: Implements tests
         [Fact]
-        public void PrintTest()
+        public Task Test0()
         {
-            _testOutputHelper.WriteLine("Hello");
-            Assert.True(true);
-        }
-
-        [Fact]
-        public void PlayerLoaded()
-        {
-            IPlayer player = _cluster.GrainFactory.GetGrain<IPlayer>(new Guid());
-            Assert.NotNull(player);
-        }
-
-        [Fact]
-        public async void PlayerInitialization()
-        {
-            List<Guid> players = new List<Guid>() { new Guid(), new Guid() };
-            IPlayer player1 = _cluster.GrainFactory.GetGrain<IPlayer>(players[0]);
-            IPlayer player2 = _cluster.GrainFactory.GetGrain<IPlayer>(players[1]);
-            await player1.Initialize(players, true);
-            await player2.Initialize(players, false);
-            Assert.True(true);
-        }
-
-        [Fact]
-        public async void PlayerRecieveBall()
-        {
-            IPlayer player = _cluster.GrainFactory.GetGrain<IPlayer>(new Guid());
-            Guid newBall = new Guid();
-            await player.ReceiveBall(newBall);
-            List<Guid> balls = await player.GetBallIds();
-            Assert.Contains(newBall, balls);
+            throw new NotImplementedException();
         }
     }
 }
